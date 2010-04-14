d868c542c0da2e810b13bfbcc544a084d139f1c3
fhen <fhen@fh-new.(none)>
2010-04-15 00:59:10 +0400

    simple debug middleware (used code from Django)

---
+++ insanities/ext/debug/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+from insanities.web.core import Wrapper
+from insanities.web.http import HttpException
+from debug_dj import technical_500_response
+
+class Debug(Wrapper):
+    def handle(self, rctx):
+        try:
+            rctx = self.next(rctx)
+        except Exception, e:
+            import httplib
+            import logging
+            logger = logging.getLogger(__name__)
+
+            rctx.response.status = httplib.INTERNAL_SERVER_ERROR
+            exc_info = sys.exc_info()
+            html = technical_500_response(rctx, *exc_info)
+            rctx.response.write(html)
+            logger.exception(e)
+        return rctx
