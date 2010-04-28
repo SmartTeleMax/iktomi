ff13777f43d913c7df0adf86e5715569179ffe00
harut <harut@harut.(none)>
2010-04-28 18:56:23 +0400

    code cleanup in gettext module

--- insanities/ext/gettext/__init__.py
+++ insanities/ext/gettext/__init__.py
@@ -12,6 +12,7 @@ def N_(msg):
     '''gettext marker'''
     return msg
 
+
 class M_(unicode):
     def __new__(cls, single, plural, multiple_by=None):
         self = unicode.__new__(cls, single)
@@ -19,47 +20,18 @@ class M_(unicode):
         self.multiple_by = multiple_by
         return self
 
-# global translations cache
-_translations = {}
-
-def translation(localepath, language, default_language, domain):
-    """
-    Returns a translation object.
-
-    It will construct a object for the requested language and add a fallback
-    to the default language, if it's different from the requested language.
-    """
-    global _translations
-    
-    # XXX normalize locale
-
-    res = _translations.get(language, None)
-    if res is not None:
-        return res
-
-    if os.path.isdir(localepath):
-        try:
-            res = gettext.translation(domain, localepath, [language])
-        except IOError, e:
-            pass
-
-    if res is None and language != default_language:
-        res = translation(localepath, default_language, default_language, domain)
-    elif res is None:
-        return gettext.NullTranslations()
-
-    _translations[language] = res
-
-    return res
-
-
-def _apply_translation_to_rctx(rctx, language, translation):
-    rctx.language = language
-    rctx.data['gettext'] = translation.ugettext
-    rctx.data['ngettext'] = translation.ungettext
-
 
 class LanguageSupport(RequestHandler):
+    """
+    Request handler addding support of i18n
+
+    :*languages* - languages ("en", "ru") or locales ("en_GB", "ru_RU") code.
+    The first language is default.
+
+    :*localepath* - a path to locale directory containing .mo file.
+
+    :*domain* - gettext domain of translation
+    """
 
     def __init__(self, languages, localepath, domain='insanities'):
         super(LanguageSupport, self).__init__()
@@ -67,18 +39,50 @@ class LanguageSupport(RequestHandler):
         self.default_language = languages[0]
         self.localepath = localepath
         self.domain = domain
+        self.translation_set = {}
         
     def handle(self, rctx):
-        rctx.languages = self.languages
-        rctx.default_language = self.default_language
-        rctx.localepath = self.localepath
-        rctx.localedomain = self.domain
-        rctx.translation = translation(self.localepath,
-                                       self.default_language,
-                                       self.default_language,
-                                       self.domain)
-        _apply_translation_to_rctx(rctx, self.default_language, rctx.translation)
+        rctx._language_handler = self
+        self.activate(rctx, self.default_language)
         raise ContinueRoute(self)
+        
+    def get_translation(self, language):
+        """
+        Returns a translation object.
+
+        Adds a fallback to language without locale, if a language is particular
+        locale and has not translation.
+
+        Adds a fallback to the default language, if it's
+        different from the requested language.
+        
+        :*language* - language ("en", "ru") or locale ("en_GB", "ru_RU") code.
+        """
+        # XXX normalize locale
+
+        res = self.translation_set.get(language)
+        if res is not None:
+            return res
+
+        if os.path.isdir(self.localepath):
+            try:
+                res = gettext.translation(self.domain, self.localepath, [language])
+            except IOError, e:
+                if '_' in language:
+                    res = self.get_translation(language.split('_')[0])
+                elif language != self.default_language:
+                    res = self.get_translation(self.default_language)
+                else:
+                    return gettext.NullTranslations()
+
+        self.translation_set[language] = res
+        return res
+
+    def activate(self, rctx, language):
+        rctx.translation = self.get_translation(self.language)
+        rctx.language = language
+        rctx.data['gettext'] = rctx.translation.ugettext
+        rctx.data['ngettext'] = rctx.translation.ungettext
 
 
 class set_lang(RequestHandler):
@@ -96,12 +100,9 @@ class set_lang(RequestHandler):
         self.language = language
 
     def handle(self, rctx):
-        rctx.translation = translation(rctx.localepath,
-                                       self.language,
-                                       rctx.default_language,
-                                       rctx.localedomain)
-        _apply_translation_to_rctx(rctx, self.language, rctx.translation)
+        rctx._language_handler.activate(rctx, self.language)
         return rctx
+
 
 class FormEnvironmentMixin(object):
     '''
