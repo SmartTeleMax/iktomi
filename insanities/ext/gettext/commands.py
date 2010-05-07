dd4938fffd0673dec18dc0db7e1f9e2ec6b6fd1e
harut <harut@harut.(none)>
2010-05-07 20:25:14 +0400

    moved helpers to command class

--- insanities/ext/gettext/commands.py
+++ insanities/ext/gettext/commands.py
@@ -13,16 +13,36 @@ plural_forms_re = re.compile(r'^(?P<value>"Plural-Forms.+?\\n")\s*$', re.MULTILI
 
 class gettext_commands(CommandDigest):
     ''''''
+    # XXX staff from Django. need to be refactored
+    def _popen(self, cmd):
+        """
+        Friendly wrapper around Popen for Windows
+        """
+        p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE,
+                  close_fds=os.name != 'nt', universal_newlines=True)
+        return p.communicate()
+
+    def is_ignored(self, path, ignore_patterns):
+        """
+        Helper function to check if the given path should be ignored or not.
+        """
+        import fnmatch
+        for pattern in ignore_patterns:
+            if pattern and fnmatch.fnmatchcase(path, pattern):
+                return True
+        return False
 
     def check_gettext(self):
         # We require gettext version 0.15 or newer.
-        output = _popen('xgettext --version')[0]
+        output = self._popen('xgettext --version')[0]
         match = re.search(r'(?P<major>\d+)\.(?P<minor>\d+)', output)
         if match:
             xversion = (int(match.group('major')), int(match.group('minor')))
             if xversion < (0, 15):
-                raise Exception("Internationalization requires GNU gettext 0.15 or newer. You are using version %s, please upgrade your gettext toolset." % match.group())
-    
+                raise Exception("Internationalization requires GNU gettext'
+                ' 0.15 or newer. You are using version %s, please upgrade '
+                'your gettext toolset." % match.group())
+
     def find_files(self, root, ignore_patterns, verbosity):
         """
         Helper function to get all files in the given root.
@@ -31,14 +51,14 @@ class gettext_commands(CommandDigest):
         for (dirpath, dirnames, filenames) in os.walk(root):
             for f in filenames:
                 norm_filepath = os.path.normpath(os.path.join(dirpath, f))
-                if is_ignored(norm_filepath, ignore_patterns):
+                if self.is_ignored(norm_filepath, ignore_patterns):
                     if verbosity > 1:
                         sys.stdout.write('ignoring file %s in %s\n' % (f, dirpath))
                 else:
                     all_files.extend([(dirpath, f)])
         all_files.sort()
         return all_files
-    
+
     def command_make(self, locale=None, localedir=None, domain='insanities', verbosity='1',
             extensions='html', ignore='', searchdir='.'):
         """
@@ -54,16 +74,16 @@ class gettext_commands(CommandDigest):
         ignore_patterns = ['*/.*', '*~']
         if ignore:
             ignore_patterns += ignore.split(';')
-        
+
         if localedir is None:
             localedir = os.path.abspath('locale')
-     
+
         if locale is None or domain is None:
             sys.stdout.write(self.__class__.command_make.__doc__)
             raise Exception() # what exception we need to raise?
-    
+
         self.check_gettext()
-        
+
         basedir = os.path.join(localedir, locale, 'LC_MESSAGES')
         if not os.path.isdir(basedir):
             os.makedirs(basedir)
@@ -73,17 +93,17 @@ class gettext_commands(CommandDigest):
 
         if os.path.exists(potfile):
             os.unlink(potfile)
-            
+
         for dirpath, file in self.find_files(searchdir, ignore_patterns, verbosity):
             file_base, file_ext = os.path.splitext(file)
-            
+
             if file_ext == '.py' or file_ext[1:] in extensions:
                 if verbosity > 1:
                     sys.stdout.write('processing file %s in %s\n' % (file, dirpath))
-                
+
                 cmd = 'xgettext -d %s -L Python --keyword=N_ --keyword=M_:1,2 --from-code UTF-8 -o - "%s"' % (
                     domain, os.path.join(dirpath, file))
-                msgs, errors = _popen(cmd)
+                msgs, errors = self._popen(cmd)
                 if errors:
                     raise Exception("errors happened while running xgettext on %s\n%s" % (file, errors))
 
@@ -96,12 +116,12 @@ class gettext_commands(CommandDigest):
                     open(potfile, 'ab').write(msgs)
 
         if os.path.exists(potfile):
-            msgs, errors = _popen('msguniq --to-code=utf-8 "%s"' % potfile)
+            msgs, errors = self._popen('msguniq --to-code=utf-8 "%s"' % potfile)
             if errors:
                 raise Exception("errors happened while running msguniq\n%s" % errors)
             open(potfile, 'w').write(msgs)
             if os.path.exists(pofile):
-                msgs, errors = _popen('msgmerge -q "%s" "%s"' % (pofile, potfile))
+                msgs, errors = self._popen('msgmerge -q "%s" "%s"' % (pofile, potfile))
                 if errors:
                     raise Exception("errors happened while running msgmerge\n%s" % errors)
             open(pofile, 'wb').write(msgs)
@@ -118,17 +138,17 @@ class gettext_commands(CommandDigest):
         """
         import polib
         cfg = self.cfg
-        
+
         if localedir is None:
             localedir = os.path.abspath('locale')
 
         if locale is None:
             sys.stdout.write(self.__class__.command_compile.__doc__)
             raise Exception() # what exception we need to raise?
-        
+
         result = polib.pofile(cfg.LOCALE_FILES[0] % locale)
         plural = result.metadata.get('Plural-Forms')
-        
+
         if '_' in locale:
             locales = (locale, locale.split('_')[0])
         else:
@@ -141,16 +161,16 @@ class gettext_commands(CommandDigest):
                     sys.stdout.write('skipping file %s\n' % file)
                     continue
                 pofile = polib.pofile(file)
-                
+
                 for entry in pofile:
                     old_entry = result.find(entry.msgid, by='msgid')
-                    
+
                     if old_entry is None:
                         result.append(entry)
                     elif not old_entry.msgstr and entry.msgstr:
                         # XXX check if it is correct
                         old_entry.msgstr = entry.msgstr
-                
+
                 new_plural = pofile.metadata.get('Plural-Forms')
                 if not plural and new_plural:
                     result.metadata['Plural-Forms'] = new_plural
@@ -159,25 +179,8 @@ class gettext_commands(CommandDigest):
         if not os.path.isdir(os.path.dirname(out_path)):
             os.makedirs(os.path.dirname(out_path))
         result.save_as_mofile(out_path) #are plural expressions saved correctly?
+
         if dbg:
             out_path = os.path.join(localedir, locale, 'LC_MESSAGES/_dbg.po')
             result.save(out_path)
 
-
-# XXX staff from Django. need to be refactored
-def _popen(cmd):
-    """
-    Friendly wrapper around Popen for Windows
-    """
-    p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE, close_fds=os.name != 'nt', universal_newlines=True)
-    return p.communicate()
-
-def is_ignored(path, ignore_patterns):
-    """
-    Helper function to check if the given path should be ignored or not.
-    """
-    import fnmatch
-    for pattern in ignore_patterns:
-        if pattern and fnmatch.fnmatchcase(path, pattern):
-            return True
-    return False
