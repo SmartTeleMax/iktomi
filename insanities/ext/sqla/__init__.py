9a67d3f7e00ff7dc06fc3156c6396150df3fdce0
Harut <genotip7@gmail.com>
2010-05-10 20:49:45 +0400

    small fixes in paginator in sqlalchemy

--- insanities/ext/sqla/__init__.py
+++ insanities/ext/sqla/__init__.py
@@ -1,9 +1,9 @@
 
 from sqlalchemy import orm, types, create_engine
 from sqlalchemy.ext import declarative
+from sqlalchemy.orm.query import Query
 
 from insanities.utils import cached_property
-
 from insanities.web import HttpException, ContinueRoute, Wrapper
 
 
@@ -26,19 +26,19 @@ class DBSession(orm.session.Session):
 
 
 class SqlAlchemy(Wrapper):
-    
+
     def __init__(self, uris, models, query_cls=Query, class_=DBSession,
                  engine_params={}):
         super(SqlAlchemy, self).__init__()
-        
+
         db_dict = {}
         for ref, uri in uris.items():
             models_module = ref and getattr(models, ref) or models
             metadata = models_module.metadata
-            
+
             engine = create_engine(uri, **engine_params)
             engine.logger.name += '(%s)' % ref
-            
+
             for table in metadata.sorted_tables:
                 db_dict[table] = engine
             for model in getattr(metadata, '_mapped_models', []):
@@ -46,7 +46,7 @@ class SqlAlchemy(Wrapper):
         self.maker = orm.sessionmaker(class_=class_, query_cls=query_cls,
                                       binds=db_dict, autoflush=False,
                                       autocommit=False)
-    
+
     def handle(self, rctx):
         # XXX should be lazy
         rctx.db = self.maker()
--- insanities/forms/form.py
+++ insanities/forms/form.py
@@ -15,9 +15,9 @@ class Form(object):
 
     '''
     Object designed to validate and convert data passed throught HTTP
-    
+
     To use it you must specify an ancestor class, like this::
-    
+
         class MyForm(Form):
             fields=[
                   fields.Field(name='input2',
@@ -28,13 +28,13 @@ class Form(object):
             medias = [media.FormJSRef('field_buttons.js'),
                       media.FormCSSRef('field_buttons.css'),]
             permissions = 'rw'
-    
+
     The constructor accepts folowing parameters:
-    
+
     :*env* - FormEnvironment instance, which keeps usefull stuff.
     There is only one required method to be implemented in FormEnvironment:
     FormEnvironment.render(template_name, **kwargs)
-    
+
     :*initial* - dictionary containing initial data for the form.
 
     :*name* - name of the form. Used asname attribute of form HTML tag,
@@ -56,7 +56,7 @@ class Form(object):
         self.python_data = initial.copy()
         # clone all fields
         self.fields = [field(parent=self) for field in self.fields]
-        
+
         if permissions is None:
             # to allow permissions definition in Form class
             permissions = self.permissions
@@ -83,7 +83,7 @@ class Form(object):
     def get_data(self, compact=True):
         '''
         Fills form data into new MultiDict.
-        
+
         If compact is True, includes only not-None values
         '''
         data = MultiDict()
@@ -135,21 +135,21 @@ class Form(object):
         Takes data (usually, request.POST) MultiDict and optionally files
         MultiDict. Validates given data using form's fields' accept method.
         If there are validation errors, collects them for future display.
-        
+
         If particular field has no edit permissions, disallows it's editing.
-        
+
         Provides an additional interface for field validation using form's
         :meth:`clean__%s` methods. This tests are started after accepting all
         of fields' data. This interface is useful for validation of couple of
         fields. Here is example it's of usage::
-        
+
             class MyForm(form.Form):
                 def clean__fieldname(self, value):
                     othervalue = self.python_data['otherfield']
                     if not mytest(value, othervalue):
                         raise convs.ValidationError("I don't like it")
                     return value
-        
+
         Returns True if the form is valid
         '''
         self.data = MultiDict(data)
