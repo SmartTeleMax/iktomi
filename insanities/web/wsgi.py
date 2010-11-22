ff02a3261f9be03602dd7263c6d1e5bc3c4836d2
Tim Perevezentsev <riffm2005@gmail.com>
2010-11-22 15:58:28 +0300

    wsgi module updates

--- insanities/web/wsgi.py
+++ insanities/web/wsgi.py
@@ -10,15 +10,15 @@ from .core import STOP, RequestContext
 logger = logging.getLogger(__name__)
 
 
-def process_http_exception(rctx, e):
-    rctx.response.status = e.status
+def process_http_exception(response, e):
+    response.status = e.status
     if e.status in (httplib.MOVED_PERMANENTLY,
                     httplib.SEE_OTHER):
         if isinstance(e.url, unicode):
             url = e.url.encode('utf-8')
         else:
             url = str(e.url)
-        rctx.response.headers.add('Location', url)
+        response.headers.add('Location', url)
 
 
 class WSGIHandler(object):
@@ -31,17 +31,18 @@ class WSGIHandler(object):
 
     def __call__(self, env, start_response):
         rctx = RequestContext(env)
+        response = rctx.response
         try:
             result = self.app(rctx)
             if result is STOP:
-                rctx.response.status = httplib.NOT_FOUND
+                response.status = httplib.NOT_FOUND
             else:
                 rctx = result
         except HttpException, e:
-            process_http_exception(rctx, e)
+            process_http_exception(response, e)
         except Exception, e:
             logger.exception(e)
             raise
-        headers = rctx.response.headers.items()
-        start_response(rctx.response.status, headers)
-        return [rctx.response.body]
+        headers = response.headers.items()
+        start_response(response.status, headers)
+        return [response.body]
