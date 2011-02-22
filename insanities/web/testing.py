e06b4988de3b1811dfc15a301bad47891a3ea61b
Tim Perevezentsev <riffm2005@gmail.com>
2011-02-22 20:38:10 +0300

    additional_env and additional_data kwargs in ask()

--- insanities/web/testing.py
+++ insanities/web/testing.py
@@ -6,9 +6,10 @@ from .http import Request
 from ..utils.storage import VersionedStorage
 
 
-def ask(application, url, method='get', data=None, headers=None):
-    env = VersionedStorage()
+def ask(application, url, method='get', data=None,
+        headers=None, additional_env=None, additional_data=None):
+    env = VersionedStorage(additional_env or {})
     #TODO: may be later process cookies separatly
     env.request = Request.blank(url, POST=data, headers=headers)
-    data = VersionedStorage()
+    data = VersionedStorage(additional_data or {})
     return application(env, data)
