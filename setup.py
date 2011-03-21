582735a7303c5feb05019b7408981b3f350a628d
Tim Perevezentsev <riffm2005@gmail.com>
2011-03-21 20:51:48 +0300

    `setup.py` fixes

--- setup.py
+++ setup.py
@@ -3,26 +3,29 @@
 from distutils.core import setup
 
 setup(
-    name="insanities",
-    version="0.2.1",
-    packages=["insanities","insanities.utils","insanities.forms",
-              "insanities.web","insanities.ext","insanities.ext.jinja2",
-              "insanities.ext.debug"], # is last package ok?
+    name='insanities',
+    version='0.3',
+    packages=['insanities', 'insanities.utils', 'insanities.forms',
+              'insanities.web', 'insanities.templates', 'insanities.templates.mint',
+              'insanities.templates.jinja2'],
+    package_dir={
+        'insanities.templates.jinja2':'insanities/templates/jinja2',
+        'insanities.templates.mint':'insanities/templates/mint',
+    },
     package_data={
-        '':["templates/*/*.html"]
+        'insanities.templates.jinja2':['templates/*/*.html'],
+        'insanities.templates.mint':['templates/*/*.mint'],
     },
     requires=[
-        "webob",
-        "simplejson",# json encode, decode tool
-        "html5lib",
-        "mage", # commands support
+        'webob',
     ],
-    author="Denis Otkidach",
-    author_email="denis.otkidach@gmail.com",
-    maintainer="Tim Perevezentsev",
-    maintainer_email="riffm2005@gmail.com",
-    description="A set of insanities of several geeks to show ourselves our coolness.",
-    url="http://github.com/riffm/insanities-testing/",
-    license="MIT",
-    keywords="web forms",
+    author='Denis Otkidach',
+    author_email='denis.otkidach@gmail.com',
+    maintainer='Tim Perevezentsev',
+    maintainer_email='riffm2005@gmail.com',
+    description='A set of insanities of several geeks to show ourselves our coolness.',
+    long_description=open('README').read(),
+    url='http://github.com/riffm/insanities-testing/',
+    license='MIT',
+    keywords='web forms',
 )
