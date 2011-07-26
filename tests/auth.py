01be6e4cd3edb4d8246a8c1db58da7cddfa7f613
Tim Perevezentsev <riffm2005@gmail.com>
2011-07-26 13:23:27 +0400

    moar tests for `Auth`!

--- tests/auth.py
+++ tests/auth.py
@@ -15,7 +15,8 @@ class MockUser(object):
 
 
 def get_user_identity(env, login, password):
-    return 'user-identity'
+    if password == '123':
+        return 'user-identity'
 
 
 def identify_user(env, user_identity):
@@ -47,6 +48,9 @@ class CookieAuthTests(unittest.TestCase):
         )
         root = web.Reverse.from_handler(self.app)
 
+    def login(self, login, password):
+        return web.ask(self.app, '/login', data={'login':login, 'password':password})
+
     def test_anonymouse(self):
         '`Auth` anonymouse access'
         response = web.ask(self.app, '/a')
@@ -55,3 +59,35 @@ class CookieAuthTests(unittest.TestCase):
         response = web.ask(self.app, '/b')
         self.assertEqual(response.status_int, 303)
         self.assertEqual(response.headers['Location'], '/login?next=/b')
+
+    def test_login(self):
+        '`Auth` login of valid user'
+        response = self.login('user name', '123')
+        self.assertEqual(response.status_int, 303)
+        self.assertEqual(response.headers['Location'], '/')
+        self.assert_('Set-Cookie' in response.headers)
+
+    def test_protected_resource(self):
+        '`Auth` requesting protected resource by logined user'
+        response = self.login('user name', '123')
+        response = web.ask(self.app, '/b', headers={'Cookie': response.headers['Set-Cookie']})
+        self.assertEqual(response.status_int, 200)
+
+    def test_logout_anonymouse(self):
+        '`Auth` logout of anonymouse'
+        response = web.ask(self.app, '/logout', data={})
+        self.assertEqual(response, None)
+
+    def test_logout_by_get(self):
+        '`Auth` logout by GET metod'
+        response = web.ask(self.app, '/logout')
+        self.assertEqual(response, None)
+
+    def test_logout(self):
+        '`Auth` logout of logined user'
+        response = self.login('user name', '123')
+        response = web.ask(self.app, '/logout', data={},
+                           headers={'Cookie': response.headers['Set-Cookie']})
+        self.assertEqual(response.status_int, 303)
+        self.assertEqual(response.headers['Location'], '/')
+        self.assert_(response.headers['Set-Cookie'].startswith('auth=; Max-Age=0; Path=/;'))
