b92473f843e3d7946e005302e44a5ddf04e60ec9
Tim Perevezentsev <riffm2005@gmail.com>
2013-03-05 15:19:14 +0400

    using waitpid when stopping fcgi process

--- iktomi/cli/fcgi.py
+++ iktomi/cli/fcgi.py
@@ -61,19 +61,39 @@ class Flup(Cli):
                      logfile=self.logfile, daemonize=daemonize,
                      cwd=self.cwd, umask=self.umask, **self.fastcgi_params)
 
+    def _wait_pid(self, pid):
+        try:
+            p, status = os.waitpid(pid, os.WNOHANG)
+            if p != 0:
+                if os.WIFSIGNALED(status):
+                    logger.warning('Fcgi (PID=%d) killed with signal %d',
+                                   p, os.WTERMSIG(status))
+                    sys.exit()
+                elif os.WIFEXITED(status):
+                    code = os.WEXITSTATUS(status)
+                    logger.log(logging.WARNING if code else logging.INFO,
+                               'Fcgi (PID=%d) exited with status %d',
+                               p, code)
+                    sys.exit(code)
+        except OSError, exc:
+            # "No such process" is OK, silently ignore it. The rest
+            # should be logged at least.
+            if exc.errno == errno.ECHILD:
+                sys.exit('Fcgi (PID=%d) disappeared' % pid)
+            raise
+
     def command_stop(self):
         if self.pidfile:
             if not os.path.exists(self.pidfile):
                 sys.exit('Pidfile %r is absent\n' % self.pidfile)
             with open(self.pidfile) as pidfile:
                 pid = int(pidfile.read())
-            try:
-                os.kill(pid, signal.SIGTERM)
-            except OSError, err:
-                if err.errno != errno.ESRCH:
-                    raise
-            print 'Process %r stopped' % pid
-            return
+            os.kill(pid, signal.SIGINT)
+            time.sleep(2)
+            self._wait_pid(pid)
+            os.kill(pid, signal.SIGKILL)
+            time.sleep(3)
+            self._wait_pid(pid)
         sys.exit('No pidfile provided\n')
 
     def command_restart(self):
