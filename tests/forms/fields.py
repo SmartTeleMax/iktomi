ec3382a379fa51803938ee688f08862c5c1a9471
Harut <yes@harutune.name>
2013-04-02 19:53:07 +0400

    tests for get_field #122

--- tests/forms/fields.py
+++ tests/forms/fields.py
@@ -15,3 +15,25 @@ class FieldTests(unittest.TestCase):
         form = _Form()
         self.assert_(form.accept(MultiDict(input='value')))
         self.assertEqual(form.python_data['input'], 'value')
+
+    def test_get_field(self):
+        class F(Form):
+            fields = [
+                FieldSet('fieldset', fields=[
+                    FieldList('fieldlist',
+                        field=FieldSet(None, fields=[
+                            Field('subfield')
+                        ])
+                    )
+                ])
+            ]
+
+        form = F()
+        for nm, cls in [('fieldset', FieldSet),
+                        ('fieldset.fieldlist', FieldList),
+                        ('fieldset.fieldlist-1', FieldSet),
+                        ('fieldset.fieldlist-1.subfield', Field),
+                        ]:
+            self.assert_(isinstance(form.get_field(nm), cls),
+                         '%s is not instance of %s' % (nm, cls))
+            self.assertEqual(form.get_field(nm).input_name, nm)
