ba1d68d5a827452550d72ac8c2eae987e0e573f1
Denis Otkidach <denis.otkidach@gmail.com>
2013-04-23 17:09:46 +0400

    `deprecated` as both decorator and decorator factory

--- iktomi/utils/deprecation.py
+++ iktomi/utils/deprecation.py
@@ -1,13 +1,21 @@
 import warnings, functools
 
-def deprecated(func):
+def deprecated(func_or_comment):
     '''This is a decorator which can be used to mark functions
     as deprecated. It will result in a warning being emitted
     when the function is used.'''
-    @functools.wraps(func)
-    def new_func(*args, **kwargs):
-        warnings.warn("Call to deprecated function %s." % func.__name__,
-                      category=DeprecationWarning)
-        return func(*args, **kwargs)
-    return new_func
-
+    def deco(func):
+        @functools.wraps(func)
+        def new_func(*args, **kwargs):
+            message = "Call to deprecated function %s." % func.__name__
+            if comment is not None:
+                message += ' ' + comment
+            warnings.warn(message, category=DeprecationWarning)
+            return func(*args, **kwargs)
+        return new_func
+    if isinstance(func_or_comment, basestring):
+        comment = func_or_comment
+        return deco
+    else:
+        comment = None
+        return deco(func_or_comment)
