fea43e15f43748d4ad5e5fd8d3cb00189ad8530e
Denis Otkidach <denis.otkidach@gmail.com>
2013-04-26 11:56:00 +0400

    Email.regex is simplified; thorough comment about non-text characters is added

--- iktomi/unstable/forms/convs.py
+++ iktomi/unstable/forms/convs.py
@@ -8,9 +8,16 @@ class Email(Char):
     regex = re.compile(
         # dot-atom
         r"(^[-!#$%&'*+/=?^_`{}|~0-9A-Z]+(\.[-!#$%&'*+/=?^_`{}|~0-9A-Z]+)*"
-        # quoted-string 
-        r'|^"([\001-\010\013\014\016-\037!#-\[\]-\177]|'
-            r'\\[\001-011\013\014\016-\177])*"'
+        # Although quoted variant is allowed by spec it's actually not used
+        # except by geeks that are looking for problems. But the characters
+        # allowed in quoted string are not safe for HTML and XML, so quoted
+        # e-mail can't be expressed in such formats directly which is quite
+        # common. We prefer to forbid such valid but unsafe e-mails to avoid
+        # security problems. To allow quoted names disable non-text characters
+        # replacement and uncomment the following lines of regexp:
+        ## quoted-string
+        #r'|^"([\001-\010\013\014\016-\037!#-\[\]-\177]|'
+        #    r'\\[\001-011\013\014\016-\177])*"'
         r')@(?:[A-Z0-9-]+\.)+[A-Z]{2,6}$', re.IGNORECASE)
     error_regex = N_('incorrect e-mail address')
 
