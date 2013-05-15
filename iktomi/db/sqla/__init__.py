9fbb77afafe41367c03637dd05b73a35a5e36a61
Denis Otkidach <denis.otkidach@gmail.com>
2013-05-15 19:53:34 +0400

    SQLAlchemy types are moved to separate module

--- iktomi/db/sqla/__init__.py
+++ iktomi/db/sqla/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import logging
-from sqlalchemy import orm, types, create_engine
+from sqlalchemy import orm, create_engine
 from sqlalchemy.orm.query import Query
 from iktomi.utils import cached_property, import_string
 from iktomi.utils.deprecation import deprecated
@@ -38,77 +38,3 @@ def session_maker(databases, query_cls=Query, models_location='models',
             binds[table] = engine
     return orm.sessionmaker(class_=session_class, query_cls=query_cls,
                             binds=binds, **session_params)
-
-
-class StringList(types.TypeDecorator):
-
-    impl = types.String
-
-    def process_bind_param(self, value, dialect):
-        if value is not None:
-            return ','.join(value)
-
-    def process_result_value(self, value, dialect):
-        if value is not None:
-            return filter(None, value.split(','))
-
-
-class IntegerList(types.TypeDecorator):
-
-    impl = types.String
-
-    def process_bind_param(self, value, dialect):
-        if value is not None:
-            return ','.join(str(item) for item in value)
-
-    def process_result_value(self, value, dialect):
-        if value is not None:
-            return [int(item) for item in value.split(',') if item]
-
-
-try:
-    from jinja2 import Markup
-except ImportError:
-    pass
-else:
-    Markup = None
-
-
-class HtmlBase(types.TypeDecorator):
-    '''Base class for HTML markup types (safe to render in template)'''
-
-    markup_class = Markup
-
-    def process_result_value(self, value, dialect):
-        if value is not None:
-            return self.markup_class(value)
-
-    def process_bind_param(self, value, dialect):
-        if value is not None:
-            return unicode(value)
-
-
-class Html(HtmlBase):
-    '''
-    Factory class for HTML type. Usage:
-        Column(Html(Text))
-        Column(Html(String(1000)))
-        Column(Html(BigText, markup_class=SomeWrapperClass))
-    '''
-
-    def __init__(self, _impl, markup_class=Markup):
-        if callable(_impl):
-            _impl = _impl()
-        self.impl = _impl
-        self.markup_class = markup_class
-        # Don't call base class' __init__ since we reimplemented it in a
-        # different way.
-
-
-# Compatibility classes. Deprecate them?
-
-class HtmlString(HtmlBase):
-    impl = types.String
-
-class HtmlText(HtmlBase):
-    impl = types.Text
---
+++ iktomi/db/sqla/types.py
@@ -0,0 +1,75 @@
+from sqlalchemy.types import TypeDecorator, String, Text
+
+
+class StringList(TypeDecorator):
+
+    impl = String
+
+    def process_bind_param(self, value, dialect):
+        if value is not None:
+            return ','.join(value)
+
+    def process_result_value(self, value, dialect):
+        if value is not None:
+            return filter(None, value.split(','))
+
+
+class IntegerList(TypeDecorator):
+
+    impl = String
+
+    def process_bind_param(self, value, dialect):
+        if value is not None:
+            return ','.join(str(item) for item in value)
+
+    def process_result_value(self, value, dialect):
+        if value is not None:
+            return [int(item) for item in value.split(',') if item]
+
+
+try:
+    from jinja2 import Markup
+except ImportError:
+    pass
+else:
+    Markup = None
+
+
+class HtmlBase(TypeDecorator):
+    '''Base class for HTML markup types (safe to render in template)'''
+
+    markup_class = Markup
+
+    def process_result_value(self, value, dialect):
+        if value is not None:
+            return self.markup_class(value)
+
+    def process_bind_param(self, value, dialect):
+        if value is not None:
+            return unicode(value)
+
+
+class Html(HtmlBase):
+    '''
+    Factory class for HTML type. Usage:
+        Column(Html(Text))
+        Column(Html(String(1000)))
+        Column(Html(BigText, markup_class=SomeWrapperClass))
+    '''
+
+    def __init__(self, _impl, markup_class=Markup):
+        if callable(_impl):
+            _impl = _impl()
+        self.impl = _impl
+        self.markup_class = markup_class
+        # Don't call base class' __init__ since we reimplemented it in a
+        # different way.
+
+
+# Compatibility classes. Deprecate them?
+
+class HtmlString(HtmlBase):
+    impl = String
+
+class HtmlText(HtmlBase):
+    impl = Text
