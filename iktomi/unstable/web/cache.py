f86ee4ce9ff8b97e813cac518ec4ed454a42cdce
shiberz <shiberz@shiberz-700Z5A.(none)>
2013-06-10 17:24:43 +0400

    added CachManagerWithContentType to unstable/cache.py, also correct __all__ property there

--- iktomi/unstable/web/cache.py
+++ iktomi/unstable/web/cache.py
@@ -4,11 +4,11 @@ XXX: Before marge to cache.py lets test as separate module for
      a while for testing and fast rollback
 
 Supports two types of cache:
-* Cache class is for basic cache that stores only content
-* CachedWithContentType is for cache with content type (as separate cache key)
+* CacheManager class is for basic cache that stores only content
+* CacheManagerWithContentType is for cache with content type (as separate cache key)
 """
 
-__all__ = ['Cached', 'CachedWithContentType', 'CachedWithContentType']
+__all__ = ['CacheManager', 'CacheManagerWithContentType', 'cache', 'nocache']
 
 from iktomi import web
 import logging
@@ -52,9 +52,9 @@ class CacheHandler(web.WebHandler):
         return cache(self._duration, self._content_type)(self.next_handler)(env, data)
 
 
-
 class CacheManager(web.WebHandler):
-    
+    _cache_name_length = 250
+
     def __init__(self, storage, default_duration, durations, content_type='text/html'):
         self._storage = storage
         self._default_duration = default_duration
@@ -64,7 +64,7 @@ class CacheManager(web.WebHandler):
 
     def get_cache_name(self, env):
         cache_name = env.request.url
-        if len(cache_name)>250:
+        if len(cache_name) > self._cache_name_length:
             return None
         return cache_name
 
@@ -132,4 +132,40 @@ class CacheManager(web.WebHandler):
 
     def nocache(self):
         return NocacheHandler()
+
+
+class CacheManagerWithContentType(CacheManager):
+    _cache_name_length = 247
+
+    def __init__(self, storage, default_duration, durations, content_type='text/html', content_type_prefix='ct:'):
+        super(CacheManagerWithContentType, self).__init__(storage, default_duration, durations, content_type)
+        self._ct_prefix = 'ct:'
+
+    def save_response_to_cache(self, env, response, duration):
+        super(CacheManagerWithContentType, self)\
+            .save_response_to_cache(env, response, duration)
+        
+        content_type = response.headers.getall('content-type')
+        if not content_type:
+            content_type = self._content_type
+        else:
+            # XXX should we check if there is only one value as it done in WebOb?
+            content_type = content_type[0]
+        logger.info('Caching Content-Type `%s` for %i seconds %s', content_type,
+                    duration, cache_name)
+        self._storage.set(self._ct_prefix + cache_name, content_type, duration)
+
+
+    def get_response_from_cache(self, env):
+        response = super(CacheManagerWithContentType, self)\
+            .get_response_from_cache(env)
+        if response is None:
+            return None
+        content_type = self._storage.get(self._ct_prefix + cache_name)
+        logger.info('Got from cache: %s , Content-Type: %s',
+                    (body if body is None else type(body)), content_type)
+        if content_type is None:
+            return None
+        response.content_type = content_type
+        return response
 
