10b0ffcb007c6e9dcfc0cc5cf5ce0bdf7f5c222a
Denis Otkidach <denis.otkidach@gmail.com>
2013-06-20 21:19:54 +0400

    AutoTableNameMeta, table_args_meta (untested)

--- iktomi/db/sqla/__init__.py
+++ iktomi/db/sqla/__init__.py
@@ -3,6 +3,7 @@
 import logging
 from sqlalchemy import orm, create_engine
 from sqlalchemy.orm.query import Query
+from sqlalchemy.ext import declarative
 from iktomi.utils import cached_property, import_string
 from iktomi.utils.deprecation import deprecated
 
@@ -45,3 +46,42 @@ def session_maker(databases, query_cls=Query, models_location='models',
             binds[table] = engine
     return orm.sessionmaker(class_=session_class, query_cls=query_cls,
                             binds=binds, **session_params)
+
+
+class AutoTableNameMeta(declarative.DeclarativeMeta):
+
+    def __init__(cls, name, bases, dict_):
+        super(AutoTableNameMeta).__init__(self, name, bases, dict_)
+        if '_decl_class_registry' in cls.__dict__:
+            # Do not extend base class
+            return
+        # Lookup __dict__ and not attribute directly since __tablename__
+        # should change on inheritance.
+        if '__tablename__' not in cls.__dict__ and \
+                not cls.__dict__.get('__abstract__', False):
+            cls.__tablename__ = classname
+        # XXX Commented untill we write the reason in comments
+        #elif cls.__tablename__ is None:
+        #    del cls.__tablename__
+
+
+def table_args_meta(table_args):
+
+    class TableArgsMeta(declarative.DeclarativeMeta):
+
+        def __init__(cls, name, bases, dict_):
+            super(TableArgsMeta).__init__(self, name, bases, dict_)
+            if '_decl_class_registry' in cls.__dict__:
+                # Do not extend base class
+                return
+            if cls.__dict__.get('__tablename__') and \
+                    not cls.__dict__.get('__abstract__', False):
+                ta = getattr(cls, '__table_args__', {})
+                if isinstance(ta, dict):
+                    ta = dict(table_args, **ta)
+                    cls.__table_args__ = ta
+                else:
+                    ta = dict(table_args, **ta[-1])
+                    cls.__table_args__ = ta[:-1] + (ta,)
+
+    return TableArgsMeta
