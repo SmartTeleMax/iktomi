919a04c6dd4d2bd2cff1e85f703a74e3f095867c
Denis Otkidach <denis.otkidach@gmail.com>
2013-06-21 14:38:41 +0400

    SQLAlchemy declarative stuff moved to separate module

---
+++ iktomi/db/sqla/declarative.py
@@ -0,0 +1,38 @@
+from sqlalchemy.ext import declarative
+
+
+class AutoTableNameMeta(declarative.DeclarativeMeta):
+
+    def __init__(cls, name, bases, dict_):
+        # Do not extend base class
+        if '_decl_class_registry' not in cls.__dict__:
+            # Lookup __dict__ and not attribute directly since __tablename__
+            # should change on inheritance.
+            if '__tablename__' not in cls.__dict__ and \
+                    not cls.__dict__.get('__abstract__', False):
+                cls.__tablename__ = name
+            # XXX Commented untill we write the reason in comments
+            #elif cls.__tablename__ is None:
+            #    del cls.__tablename__
+        super(AutoTableNameMeta, cls).__init__(name, bases, dict_)
+
+
+def table_args_meta(table_args):
+
+    class TableArgsMeta(declarative.DeclarativeMeta):
+
+        def __init__(cls, name, bases, dict_):
+            # Do not extend base class
+            if '_decl_class_registry' not in cls.__dict__:
+                if cls.__dict__.get('__tablename__') and \
+                        not cls.__dict__.get('__abstract__', False):
+                    ta = getattr(cls, '__table_args__', {})
+                    if isinstance(ta, dict):
+                        ta = dict(table_args, **ta)
+                        cls.__table_args__ = ta
+                    else:
+                        ta = dict(table_args, **ta[-1])
+                        cls.__table_args__ = ta[:-1] + (ta,)
+            super(TableArgsMeta, cls).__init__(name, bases, dict_)
+
+    return TableArgsMeta
