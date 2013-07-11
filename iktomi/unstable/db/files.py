1270c59d12e2993af9331da74582b3bfd577c775
Denis Otkidach <denis.otkidach@gmail.com>
2013-07-11 18:20:51 +0400

    Data classes for files

---
+++ iktomi/unstable/db/files.py
@@ -0,0 +1,31 @@
+'''
+Data classes representing references to files in model objects. Manager class
+for common operations with files. Manager encapsulate knowledge on where and
+how to store transient and persistent files.
+'''
+
+import os
+
+
+class BaseFile(object):
+
+    def __init__(self, root, name):
+        '''@root depends on environment of application and @name uniquely
+        identifies the file.'''
+        self.root = root
+        self.name = name
+
+    @property
+    def path(self):
+        os.path.join(self.root, self.name)
+
+    def __repr__(self):
+        return '{}({})'.format(type(self).__name__, self.name)
+
+
+class TransientFile(BaseFile):
+    pass
+
+
+class PersistentFile(object):
+    pass
