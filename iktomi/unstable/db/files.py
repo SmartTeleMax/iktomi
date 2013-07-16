044b1ba60c11e08bf1e34d353b9c9b83a7f261c1
Harut <yes@harutune.name>
2013-07-16 15:41:58 +0400

    draft file upload handling (forms and manager)

--- iktomi/unstable/db/files.py
+++ iktomi/unstable/db/files.py
@@ -5,6 +5,16 @@ how to store transient and persistent files.
 '''
 
 import os
+import cgi
+import errno
+from ..utils import cached_property
+
+
+def _get_file_content(f):
+    # XXX FieldStorage has no read()?
+    if isinstance(f, cgi.FieldStorage):
+        return f.value
+    return f.read()
 
 
 class BaseFile(object):
@@ -18,6 +28,24 @@ class BaseFile(object):
     @property
     def path(self):
         os.path.join(self.root, self.name)
+
+    @cached_property
+    def size(self):
+        try:
+            return os.path.getsize(self.full_path)
+        # Return None for non-existing file.
+        # There can be OSError or IOError (depending on Python version?), both
+        # are derived from EnvironmentError having errno property.
+        except EnvironmentError, exc:
+            if exc.errno!=errno.ENOENT:
+                raise
+
+    def delete(self):
+        if os.path.isfile(self.full_path):
+            try:
+                os.unlink(self.full_path)
+            except OSError:
+                pass
 
     def __repr__(self):
         return '{}({})'.format(type(self).__name__, self.name)
@@ -37,11 +65,42 @@ class MediaFileManager(object):
         self.transient_root = transient_root
         self.persistent_root = persistent_root
 
+    def make_transient_from_fs(self, fs):
+        '''Create TransientFile from cgi.FieldStorage'''
+        return self.make_transient(fs.file, fs.filename)
+
+    def make_transient(self, input_stream, original_name):
+        '''Create TransientFile with given input stream and file name'''
+        ext = os.path.splitext(original_name)[1]
+        transient = self.new_transient(ext)
+        if not os.path.isdir(self.transient_root):
+            os.makedirs(self.base_path)
+
+        with open(transient.path, 'wb') as fp:
+            # XXX buffer?
+            fp.write(self._get_file_content(input_stream))
+        return transient
+
     def new_transient(self, ext=''):
+        '''Creates empty TransientFile with random name and given extension'''
         name = os.urandom(8).encode('hex') + ext
         return TransientFile(self.transient_root, name)
 
+    def restore_transient(self, name):
+        '''Restores TransientFile object with given name.
+        Should be used when form is submitted with file name and no file'''
+        # security checks: basically no folders are allowed
+        if '/' in name or '\\' in name:
+            #logger.warning('Hacking attempt: submitted temp_name '\
+            #               'for FileField contains "/"')
+            raise # XXX
+        transient = TransientFile(self.transient_root, name)
+        if not os.path.isfile(transient.path):
+            raise # XXX
+        return transient
+
     def store(self, transient_file, persistent_name):
+        '''Makes PersistentFile from TransientFile'''
         persistent_file = PersistentFile(self.persistent_root, persistent_name)
         os.rename(transient_file.path, persistent_file.path)
         return persistent_file
---
+++ iktomi/unstable/forms/files.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+import logging
+from iktomi.forms import convs, widgets
+from ..forms.fields import Field, FieldSet, FileField
+
+logger = logging.getLogger(__name__)
+
+
+class FileFieldSetConv(convs.Converter):
+
+    error_inner = 'Error uploading file'
+    error_lost = 'Transient file has been lost'
+
+    @property
+    def file_cls(self):
+        return self.field.file_cls
+
+    def from_python(self, value):
+        return {'temp_name': value.filename if value and value.mode == 'temp' else None,
+                'original_name': value and value.original_name,
+                'file': value,
+                'mode': value.mode if value is not None else 'empty'}
+
+    def _to_python(self, file=None, mode=None, temp_name=None, original_name=None):
+
+        if not self._is_empty(file):
+            file = self.env.media_file_manager.make_transient_from_fs(file)
+        else:
+            file = None
+
+        if mode == 'delete':
+            if self.required:
+                raise convs.ValidationError(self.error_required)
+            return None
+
+        if mode == 'empty':
+            if not file and self.required:
+                raise convs.ValidationError(self.error_required)
+
+        elif mode == 'temp':
+            if not original_name:
+                logger.warning('Missing original_name for FileField')
+            if not temp_name:
+                logger.warning('Missing temp_name for FileField in mode "temp"')
+                raise convs.ValidationError(self.hacking)
+
+            try:
+                temp_file = self.env.media_file_manager.restore_transient(temp_name)
+            except Exception: # XXX what kind of exception?
+                raise convs.ValidationError(self.error_lost) # XXX english
+
+            if file:
+                temp_file.delete()
+            else:
+                file = temp_file
+
+        elif mode == 'existing':
+            if not file:
+                return self._existing_value
+        else:
+            logger.warning('Unknown mode submitted for FileField: %r', mode)
+            return self._existing_value
+        return file
+
+    def to_python(self, value):
+        value = self._to_python(**value)
+        #XXX Hack
+        self.field.set_raw_value(self.field.form.raw_data, 
+                                 self.from_python(value))
+        return value
+
+
+class FileFieldSet(FieldSet):
+    '''FieldSet aggregating fields required for file upload handling::
+    '''
+
+    fields = [
+            FileField('file',
+                  conv=convs.SimpleFile(),
+                  widget=widgets.FileInput()),
+            Field('mode',
+                  conv=convs.EnumChoice(choices=[('existing', ''),
+                                                 ('temp', ''),
+                                                 ('empty', ''),
+                                                 ('delete', ''),],
+                                        required=True),
+                  widget=widgets.HiddenInput),
+            Field('temp_name',
+                  conv=convs.Char(required=False),
+                  widget=widgets.HiddenInput),
+            Field('original_name',
+                  conv=convs.Char(required=False),
+                  widget=widgets.HiddenInput),
+        ]
+
+    conv = FileFieldSetConv
+
+    def __init__(self, name, conv=FileFieldSetConv, **kwargs):
+        kwargs.setdefault('fields', self.fields)
+        FieldSet.__init__(self, name, conv=conv, **kwargs)
+        #self.get_field('file').conv.required = self.conv.required
+
+    def get_initial(self):
+        return None # XXX
+
