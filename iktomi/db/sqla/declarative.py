23862d761084c3dd3ae6c2e95300807b79986277
Denis Otkidach <denis.otkidach@gmail.com>
2013-09-05 16:26:25 +0400

    table_args_meta is renamed to TableArgsMeta; more docstrings

--- iktomi/db/sqla/declarative.py
+++ iktomi/db/sqla/declarative.py
@@ -1,16 +1,49 @@
 # -*- coding: utf-8 -*-
 
+'''Declarative metaclasses to incapsulate repeated code into base class. To
+combine them you have to create a subclass. Example:
+
+    TABLE_ARGS = {
+        'mysql_engine': 'InnoDB',
+        'mysql_default charset': 'utf8',
+    }
+
+    class Meta(AutoTableNameMeta, TableArgsMeta(TABLE_ARGS)):
+        pass
+
+    Base = declarative_base(name='Base', metaclass=Meta)
+
+    class MyClass(Base):
+        …
+
+is equivalent to
+
+    class MyClass(Base):
+        __tablename__ = 'MyClass'
+        __table_args__ = {
+            'mysql_engine': 'InnoDB',
+            'mysql_default charset': 'utf8',
+        }
+        …
+'''
+
 from sqlalchemy.ext import declarative
+from ...utils.deprecation import deprecated
 
 
 class AutoTableNameMeta(declarative.DeclarativeMeta):
     '''Declarative metaclass automatically giving the same name to table as
     mapped class. Example:
+
         Base = declarative_base(name='Base', metaclass=AutoTableNameMeta)
+
         class MyClass(Base):
             …
+
     is equivalent to
+
         Base = declarative_base(name='Base')
+
         class MyClass(Base):
             __tablename__ = 'MyClass'
             …
@@ -30,27 +63,59 @@ class AutoTableNameMeta(declarative.DeclarativeMeta):
         super(AutoTableNameMeta, cls).__init__(name, bases, dict_)
 
 
-def table_args_meta(table_args):
+def TableArgsMeta(table_args):
+    '''Declarative metaclass automatically adding (merging) __table_args__ to
+    mapped classes. Example:
 
-    class TableArgsMeta(declarative.DeclarativeMeta):
+        Meta = TableArgsMeta({
+            'mysql_engine': 'InnoDB',
+            'mysql_default charset': 'utf8',
+        }
+
+        Base = declarative_base(name='Base', metaclass=Meta)
+
+        class MyClass(Base):
+            …
+
+    is equivalent to
+
+        Base = declarative_base(name='Base')
+
+        class MyClass(Base):
+            __table_args__ = {
+                'mysql_engine': 'InnoDB',
+                'mysql_default charset': 'utf8',
+            }
+            …
+    '''
+
+    class _TableArgsMeta(declarative.DeclarativeMeta):
 
         def __init__(cls, name, bases, dict_):
-            # Do not extend base class
-            if '_decl_class_registry' not in cls.__dict__:
-                if cls.__dict__.get('__tablename__') and \
-                        not cls.__dict__.get('__abstract__', False):
-                    ta = getattr(cls, '__table_args__', {})
-                    if isinstance(ta, dict):
-                        ta = dict(table_args, **ta)
-                        cls.__table_args__ = ta
+            if (    # Do not extend base class
+                    '_decl_class_registry' not in cls.__dict__ and 
+                    # Missing __tablename_ or equal to None means single table
+                    # inheritance — no table for it (columns go to table of
+                    # base class)
+                    cls.__dict__.get('__tablename__') and
+                    # Abstract class — no table for it (columns go to table[s]
+                    # of subclass[es]
+                    not cls.__dict__.get('__abstract__', False)):
+                ta = getattr(cls, '__table_args__', {})
+                if isinstance(ta, dict):
+                    ta = dict(table_args, **ta)
+                    cls.__table_args__ = ta
+                else:
+                    assert isinstance(ta, tuple)
+                    if ta and isinstance(ta[-1], dict):
+                        tad = dict(table_args, **ta[-1])
+                        ta = ta[:-1] + dict(table_args, **ta[-1])
                     else:
-                        assert isinstance(ta, tuple)
-                        if ta and isinstance(ta[-1], dict):
-                            tad = dict(table_args, **ta[-1])
-                            ta = ta[:-1] + dict(table_args, **ta[-1])
-                        else:
-                            tad = dict(table_args)
-                        cls.__table_args__ = ta + (tad,)
-            super(TableArgsMeta, cls).__init__(name, bases, dict_)
+                        tad = dict(table_args)
+                    cls.__table_args__ = ta + (tad,)
+            super(_TableArgsMeta, cls).__init__(name, bases, dict_)
 
-    return TableArgsMeta
+    return _TableArgsMeta
+
+
+table_args_meta = deprecated('Use TableArgsMeta instead.')(TableArgsMeta)
