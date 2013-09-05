db05371fe11f7a3838137aac5a4f7077f46498cc
Harut <yes@harutune.name>
2013-09-05 17:57:11 +0400

    return_locas is included to iktomi

--- iktomi/unstable/db/library.py
+++ iktomi/unstable/db/library.py
@@ -1,3 +1,28 @@
+import sys
+
+def return_locals(func):
+    '''
+        Wraps function, so it is executed and it's locals() are returned
+    '''
+
+    def wrap(*args, **kwargs):
+        frames = []
+        def tracer(frame, event, arg):
+            sys.setprofile(oldprofile)
+            frames.append(frame)
+
+        oldprofile = sys.getprofile()
+        # tracer is activated on next call, return or exception
+        sys.setprofile(tracer)
+        try:
+            func(*args, **kwargs)
+        finally:
+            sys.setprofile(oldprofile)
+        assert len(frames) == 1
+        return frames.pop(0).f_locals
+    return wrap
+
+
 class ModelLibrary(object):
 
     def __init__(self):
@@ -5,7 +30,7 @@ class ModelLibrary(object):
         self.lang_models = []
 
     def get_constructor(self, func):
-        return func
+        return return_locals(func)
 
     def register(self, *base_names, **kwargs):
         lang = kwargs.pop('lang', False)
