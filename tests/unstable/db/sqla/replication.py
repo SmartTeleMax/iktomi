d0973f3abc1d9ec159e9f10c50e08fe89b8bdc18
Denis Otkidach <denis.otkidach@gmail.com>
2013-09-11 20:00:24 +0400

    Basic test for replication is commented, more checks

--- tests/unstable/db/sqla/replication.py
+++ tests/unstable/db/sqla/replication.py
@@ -28,34 +28,43 @@ class ReplicationTests(unittest.TestCase):
             same = Column(String)
             different2 = Column(String)
         self.create_all()
+        # Prepare
         with self.db.begin():
             a1 = A1(same='s11', different1='d11')
             self.db.add(a1)
+        # Replect when replection doesn't exist
         with DBHistory(self.db) as hist, self.db.begin():
             a2 = replication.reflect(a1, A2)
-        self.assertIsNone(a2)
         for changes in [hist.created, hist.deleted, hist.updated]:
             self.assertFalse(changes)
+        self.assertIsNone(a2)
+        # Rerplicate when reflection doesn't exist
         with DBHistory(self.db) as hist, self.db.begin():
             a2 = replication.replicate(a1, A2)
+        hist.assert_created_one(A2)
         self.assertIsNotNone(a2)
         self.assertEqual(a2.id, a1.id)
         self.assertEqual(a2.same, 's11')
         self.assertIsNone(a2.different2)
-        hist.assert_created_one(A2)
         self.db.expunge(a2)
+        # Reflect when reflection exists, insure attributes are not copied
         with self.db.begin():
             a1.same = 's12'
         with DBHistory(self.db) as hist, self.db.begin():
             a2 = replication.reflect(a1, A2)
+        for changes in [hist.created, hist.deleted, hist.updated]:
+            self.assertFalse(hist.updated)
         self.assertIsNotNone(a2)
         self.assertEqual(a2.same, 's11')
+        # Rerplicate when reflection exists, insure common attributes are
+        # copied and own attributes are kept unchanged
         with self.db.begin():
             a2.same = 's21'
             a2.different2 = 'd21'
         self.db.expunge(a2)
         with DBHistory(self.db) as hist, self.db.begin():
             a2 = replication.replicate(a1, A2)
+        hist.assert_updated_one(A2)
         self.assertIsNotNone(a2)
         self.assertEqual(a2.same, 's12')
         self.assertEqual(a2.different2, 'd21')
