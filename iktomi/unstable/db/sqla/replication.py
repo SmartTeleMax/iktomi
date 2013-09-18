f1baa3c9c310a8d96ea613da1bf6ac38c329da73
Denis Otkidach <denis.otkidach@gmail.com>
2013-09-18 18:14:54 +0400

    Fixed to pass already written replication tests; ability to include to/exclude from replication manually

--- iktomi/unstable/db/sqla/replication.py
+++ iktomi/unstable/db/sqla/replication.py
@@ -14,12 +14,34 @@ replicate::
     references to independent objects are reflected.
 '''
 
+from weakref import WeakSet
 from sqlalchemy.orm import object_session
 from sqlalchemy.orm.util import identity_key
 from sqlalchemy.orm.attributes import manager_of_class
 from sqlalchemy.orm.properties import ColumnProperty, RelationshipProperty
 from sqlalchemy.orm.collections import collection_adapter
 from sqlalchemy.orm.attributes import instance_state, instance_dict
+
+
+_included = WeakSet()
+_excluded = WeakSet()
+
+
+def include(prop):
+    '''Replicate property that is normally not replicated. Right now it's
+    meaningful for one-to-many relations only.'''
+    assert isinstance(prop, RelationshipProperty)
+    _included.add(prop)
+
+def exclude(prop):
+    '''Don't replicate property that is normally replicated: ordering column,
+    many-to-one relation that is marked for replication from other side.'''
+    assert isinstance(prop, [Column, ColumnProperty, RelationshipProperty])
+    _excluded.add(prop)
+    if isinstance(prop, RelationshipProperty):
+        # Also exclude columns that participate in this relationship
+        for local, remote in prop.local_remote_pairs:
+            _excluded.add(local)
 
 
 def reflect(source, model):
@@ -31,10 +53,78 @@ def reflect(source, model):
     return db.query(model).get(ident)
 
 
+def replicate_relation(source, target, attr, target_attr):
+    if attr.property.cascade.delete_orphan:
+        filter_one = replicate
+        filter_list = replicate_filter
+    else:
+        filter_one = reflect
+        filter_list = reflect_filter
+    value = getattr(source, attr.key)
+    target_attr_model = target_attr.property.mapper.class_
+    if attr.property.uselist:
+        adapter = collection_adapter(value)
+        if adapter:
+            # Convert any collection to flat iterable
+            value = adapter.adapt_like_to_iterable(value)
+        reflection = filter_list(value, target_attr_model)
+        impl = instance_state(target).get_impl(attr.key)
+        # Set any collection value from flat list
+        impl._set_iterable(instance_state(target),
+                           instance_dict(target),
+                           reflection)
+    else:
+        reflection = filter_one(value, target_attr_model)
+        setattr(target, attr.key, reflection)
+
+
+def is_relation_replicatable(attr):
+    if attr.property in _included:
+        return True
+    elif attr.property in _excluded:
+        return False
+    elif attr.property.viewonly:
+        return False
+    elif attr.property.cascade.delete_orphan:
+        # Private, replicate
+        return True
+    elif attr.property.secondary is not None:
+        # Many-to-many. Usualy one side is short list and other is long or
+        # absent. Reflect if not dynamic, other cases should be excluded
+        # manually.
+        assert attr.property.lazy in (True, False, 'dynamic')
+        return attr.property.lazy!='dynamic'
+    else:
+        # Many-to-one, one-to-many, one-to-one. Reflect only if FK points from
+        # from this side to other.
+        # XXX Composite FKs are ignore (not replicated).
+        if len(attr.property.local_remote_pairs)!=1:
+            return False
+        local, remote = attr.property.local_remote_pairs[0]
+        for fk in local.foreign_keys:
+            if fk.column==remote:
+                return True
+        else:
+            return False
+
+
+def _column_property_in_registry(prop, registry):
+    if prop in registry:
+        return True
+    elif len(prop.columns)==1:
+        # Column() is translated to ColumnProperty with single column
+        return prop.columns[0] in registry
+    else:
+        return False
+
+
 def replicate_attributes(source, target):
     '''Replicates common SQLAlchemy attributes from the `source` object to the
     `target` object.'''
     target_manager = manager_of_class(type(target))
+    column_attrs = set()
+    relationship_attrs = set()
+    relationship_columns = set()
     for attr in manager_of_class(type(source)).attributes:
         if attr.key not in target_manager:
             # It's not common attribute
@@ -42,32 +132,24 @@ def replicate_attributes(source, target):
         target_attr = target_manager[attr.key]
         if isinstance(attr.property, ColumnProperty):
             assert isinstance(target_attr.property, ColumnProperty)
-            setattr(target, attr.key, getattr(source, attr.key))
+            column_attrs.add(attr)
         elif isinstance(attr.property, RelationshipProperty):
             assert isinstance(target_attr.property, RelationshipProperty)
-            target_attr_model = target_attr.property.argument
-            value = getattr(source, attr.key)
-            if attr.property.cascade.delete_orphan:
-                # Private, replicate
-                if attr.property.uselist:
-                    adapter = collection_adapter(value)
-                    if adapter:
-                        # Convert any collection to flat iterable
-                        value = adapter.adapt_like_to_iterable(value)
-                    reflection = replicate_filter(value, target_attr_model)
-                    impl = instance_state(target).get_impl(attr.key)
-                    # Set any collection value from flat list
-                    impl._set_iterable(instance_state(target),
-                                       instance_dict(target),
-                                       reflection)
-                else:
-                    reflection = target_attr_model()
-                    replicate_attributes(value, reflection)
-                    setattr(target, attr.key, reflection)
-            elif attr.property.secondary is not None:
-                # Many-to-many, reflect
-                reflection = reflect_filter(value, target_attr_model)
-                setattr(target, attr.key, reflection)
+            relationship_attrs.add(attr)
+            for local, remote in attr.property.local_remote_pairs:
+                relationship_columns.add(local)
+    for attr in column_attrs:
+        if _column_property_in_registry(attr.property, _excluded):
+            continue
+        elif (not _column_property_in_registry(attr.property, _included) and
+                 all(column in relationship_columns
+                     for column in attr.property.columns)):
+            continue
+        setattr(target, attr.key, getattr(source, attr.key))
+    for attr in relationship_attrs:
+        target_attr_model = target_manager[attr.key].property.argument
+        if is_relation_replicatable(attr):
+            replicate_relation(source, target, attr, target_manager[attr.key])
 
 
 def replicate(source, model):
