82dbddb8007d4ecccf2ca41798c572d038971ce4
Denis Otkidach <denis.otkidach@gmail.com>
2013-10-09 14:44:29 +0400

    declared_mixin decorator to avoid annoying series of @declared_attr-decorated methods

---
+++ iktomi/unstable/db/sqla/mixin.py
@@ -0,0 +1,56 @@
+import inspect, weakref
+from sqlalchemy.ext.declarative import declared_attr
+from iktomi.unstable.utils.functools import return_locals
+
+
+def declared_mixin(*bases):
+    '''Create mix-in class with all assignments turned into methods decorated
+    with declared_attr. Usage:
+
+        @declared_mixin
+        def FactoryFunction():
+
+    or with base mix-in class[es]:
+
+        @declared_mixin(BaseMixIn1, BaseMixIn2)
+        def FactoryFunction():
+
+    For example:
+
+        @declared_mixin
+        def WithParent():
+            parent_id = Column(ForeignKey(Parent.id))
+            parent = relationship(Parent)
+
+    is equivalent to
+
+        class WithParent(object):
+            @declared_attr
+            def parent_id(cls):
+                return Column(ForeignKey(Parent.id))
+            @declared_attr
+            def parent(cls):
+                return relationship(Parent)
+    '''
+
+    def wrapper(func):
+        attrs = weakref.WeakKeyDictionary()
+        def create_descriptor(name):
+            def get_attr(cls):
+                if cls not in attrs:
+                    # Call func only once per class
+                    attrs[cls] = return_locals(func)()
+                return attrs[cls][name]
+            get_attr.__name__ = name
+            return declared_attr(get_attr)
+        descriptors = {name: create_descriptor(name)
+                       for name in func.func_code.co_varnames}
+        return type(func.__name__, bases, descriptors)
+
+    if len(bases)==1 and not isinstance(bases[0], type):
+        # Short form (without bases) is used
+        func = bases[0]
+        bases = ()
+        return wrapper(func)
+    else:
+        return wrapper
---
+++ tests/unstable/db/sqla/mixin.py
@@ -0,0 +1,49 @@
+import unittest
+from sqlalchemy import Column, Integer, ForeignKey, create_engine
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.ext.declarative import declarative_base, declared_attr
+from iktomi.db.sqla.declarative import AutoTableNameMeta
+from iktomi.unstable.db.sqla.mixin import declared_mixin
+
+
+class Tests(unittest.TestCase):
+
+    def setUp(self):
+        self.Base = declarative_base(name='Base', metaclass=AutoTableNameMeta)
+
+    def test_short_form(self):
+        called = [0]
+        @declared_mixin
+        def MixIn():
+            called[0] += 1
+            id = Column(Integer, primary_key=True)
+            data = Column(Integer)
+        class A(self.Base, MixIn): pass
+        self.assertEqual(called[0], 1)
+        class B(self.Base, MixIn): pass
+        self.assertEqual(called[0], 2)
+        self.assertIsNot(A.id, B.id)
+
+    def test_with_base(self):
+        @declared_mixin
+        def PKMixIn():
+            id = Column(Integer, primary_key=True)
+        @declared_mixin(PKMixIn)
+        def PKAndDataMixIn():
+            data = Column(Integer)
+        class A(self.Base, PKAndDataMixIn): pass
+        class B(self.Base, PKAndDataMixIn): pass
+        self.assertIsNot(A.id, B.id)
+        self.assertIsNot(A.data, B.data)
+
+    def test_reference(self):
+        def TreeMixIn(cls):
+            @declared_mixin
+            def _TreeMixIn():
+                id = Column(Integer, primary_key=True)
+                parent_id = Column(ForeignKey(id))
+                parent = relationship(cls, remote_side=id)
+            return _TreeMixIn
+        class A(self.Base, TreeMixIn('A')): pass
+        class B(self.Base, TreeMixIn('B')): pass
+        # XXX What to test here in addition to the fact that it compiles?
