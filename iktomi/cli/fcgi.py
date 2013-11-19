c611b5725b2e0b9212dfe6b329dae2990893746e
Denis Otkidach <denis.otkidach@gmail.com>
2013-11-19 14:23:54 +0400

    Command "fcgi:stop" is rewriten (fixes bugs #147 and #148)

--- iktomi/cli/fcgi.py
+++ iktomi/cli/fcgi.py
@@ -22,13 +22,10 @@ def flup_fastcgi(wsgi_app, bind, cwd=None, pidfile=None, logfile=None,
         if os.path.isfile(pidfile):
             with open(pidfile, 'r') as f:
                 pid = int(f.read())
-            try:
-                os.kill(pid, 0)
-                sys.exit('Cant start fcgi b.c. process %r is running' % pid)
-            except OSError, err:
-                if err.errno != errno.ESRCH:
-                    raise
-                logger.info('Pidfile was pointing to nonexistent process %r' % \
+            if self._is_running(pid):
+                sys.exit('Already running (PID: %r)' % pid)
+            else:
+                logger.info('PID file was pointing to nonexistent process %r',
                             pid)
         doublefork(pidfile, logfile, cwd, umask)
     logger.info('Starting FastCGI server (flup), current working dir %r' % cwd)
@@ -61,43 +58,43 @@ class Flup(Cli):
                      logfile=self.logfile, daemonize=daemonize,
                      cwd=self.cwd, umask=self.umask, **self.fastcgi_params)
 
-    def _wait_pid(self, pid):
+    def _is_running(self, pid):
         try:
-            p, status = os.waitpid(pid, os.WNOHANG)
-            if p != 0:
-                if os.WIFSIGNALED(status):
-                    logger.warning('Fcgi (PID=%d) killed with signal %d',
-                                   p, os.WTERMSIG(status))
-                    sys.exit()
-                elif os.WIFEXITED(status):
-                    code = os.WEXITSTATUS(status)
-                    logger.log(logging.WARNING if code else logging.INFO,
-                               'Fcgi (PID=%d) exited with status %d',
-                               p, code)
-                    sys.exit(code)
+            os.kill(pid, 0)
         except OSError, exc:
-            # "No such process" is OK, silently ignore it. The rest
-            # should be logged at least.
-            if exc.errno == errno.ECHILD:
-                logger.info('Fcgi (PID=%d) disappeared' % pid)
-                sys.exit(0)
+            if exc.errno == errno.ESRCH:
+                return False
             raise
+        return True
+
+    def _terminate(self, pid, sig, timeout):
+        os.kill(pid, sig)
+        start = time.time()
+        while True:
+            if not self._is_running(pid):
+                return True
+            if time.time()-start>=timeout:
+                return False
+            time.sleep(0.1)
 
     def command_stop(self):
         if self.pidfile:
             if not os.path.exists(self.pidfile):
-                sys.exit('Pidfile %r is absent\n' % self.pidfile)
+                sys.exit("Pidfile %r doesn't exist" % self.pidfile)
             with open(self.pidfile) as pidfile:
                 pid = int(pidfile.read())
-            os.kill(pid, signal.SIGINT)
-            time.sleep(2)
-            self._wait_pid(pid)
-            os.kill(pid, signal.SIGKILL)
-            time.sleep(3)
-            self._wait_pid(pid)
-        sys.exit('No pidfile provided\n')
+            for sig in [signal.SIGINT, signal.SIGTERM, signal.SIGKILL]:
+                try:
+                    if self._terminate(pid, sig, 3):
+                        os.remove(self.pidfile)
+                        sys.exit()
+                except OSError, exc:
+                    if exc.errno != errno.ESRCH:
+                        raise
+                    elif sig == signal.SIGINT:
+                        sys.exit('Not running')
+        sys.exit('No pidfile provided')
 
     def command_restart(self):
         self.command_stop()
-        time.sleep(1)
         self.command_start()
