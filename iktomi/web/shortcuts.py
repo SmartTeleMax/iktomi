4ca10786b5df7a2569f91b03a393e6d3bc0337a5
Harut <yes@harutune.name>
2013-12-20 21:07:53 +0400

    http_error shortcut is not needed anymore

--- iktomi/web/shortcuts.py
+++ iktomi/web/shortcuts.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 import json
-from webob.exc import status_map
+from webob.exc import status_map, HTTPMethodNotAllowed
 from webob import Response
 from .core import cases
 from . import filters
 
-__all__ = ['redirect_to', 'http_error', 'to_json', 'Rule']
+__all__ = ['redirect_to', 'to_json', 'Rule']
 
 def redirect_to(endpoint, _code=303, qs=None, **kwargs):
     def handle(env, data):
@@ -15,13 +15,10 @@ def redirect_to(endpoint, _code=303, qs=None, **kwargs):
         raise status_map[_code](location=str(url))
     return handle
 
-def http_error(_code, **kwargs):
-    def handle(env, data):
-        raise status_map[_code](**kwargs)
-    return handle
 
 def to_json(data):
     return Response(json.dumps(data))
+
 
 def Rule(path, handler, method=None, name=None, convs=None):
     # werkzeug-style Rule
@@ -30,6 +27,6 @@ def Rule(path, handler, method=None, name=None, convs=None):
     h = filters.match(path, name, convs=convs)
     if method is not None:
         h = h | cases(filters.method(method),
-                      http_error(405))
+                      HTTPMethodNotAllowed)
     return h | handler
 
