78c8d031a7361a1c755031791d98ec86387aa9f7
Harut <yes@harutune.name>
2014-01-07 20:48:07 +0400

    one more ModelDictConv test

--- tests/unstable/forms/convs.py
+++ tests/unstable/forms/convs.py
@@ -14,7 +14,7 @@ def init_conv(conv, name='name'):
 
 
 
-class ConverterTests(unittest.TestCase):
+class EmailConvTests(unittest.TestCase):
 
     def test_email(self):
         conv = init_conv(convs.Email)
@@ -22,21 +22,21 @@ class ConverterTests(unittest.TestCase):
         value = conv.accept(email)
         self.assertEqual(value, email)
 
-    def test_email_strip(self):
+    def test_strip(self):
         conv = init_conv(convs.Email)
         # Vertical tab ('\v' or '\xob') is whitespace too, but it's not safe
         # for XML and HTML, so it's replaced.
         value = conv.accept(' \t\r\nname@example.com \t\r\n')
         self.assertEqual(value, u'name@example.com')
 
-    def test_email_non_text(self):
+    def test_non_text(self):
         for c in u'\x00\x08\x0B\x0C\x0E\x0F\uD800\uDFFF':
             conv = init_conv(convs.Email)
             value = conv.accept(u'name@example.com'+c)
             self.assertEqual(value, None)
             self.assertEqual(conv.field.form.errors.keys(), [conv.field.name])
 
-    def test_email_invalid(self):
+    def test_invalid(self):
         for email in ['name@com',
                       '@example.com',
                       'example.com',
@@ -51,26 +51,43 @@ class ConverterTests(unittest.TestCase):
             self.assertEqual(value, None)
             self.assertEqual(conv.field.form.errors.keys(), [conv.field.name])
 
-    def _init_modeldict_conv(self):
+
+class ModelDictConvTests(unittest.TestCase):
+
+    def _init_modeldict_conv(self, *a, **kw):
         class M(object):
+            a = b = None
             def __init__(self, **kwargs):
                 self.__dict__.update(kwargs)
+
+        kw['model'] = M
+
         class F(Form):
             fields = [
                 FieldSet('fs',
-                         conv=convs.ModelDictConv(model=M),
+                         conv=convs.ModelDictConv(*a, **kw),
                          fields=[Field('a'), Field('b')]),
             ]
         return F().get_field('fs').conv
 
-    def test_modeldict_to_python(self):
+    def test_to_python(self):
         conv = self._init_modeldict_conv()
         obj = conv.to_python({'a': 1, 'b': '2', 'c': 3})
         self.assertEqual(obj.a, 1)
         self.assertEqual(obj.b, '2')
         self.assertFalse(hasattr(obj, 'c'))
 
-    def test_modeldict_from_python(self):
+    def test_validation_error(self):
+        def validate(field, value):
+            raise convs.ValidationError('')
+        conv = self._init_modeldict_conv(validate)
+        obj = conv.accept({'a': 1, 'b': '2', 'c': 3})
+        self.assertEqual(obj.__class__.__name__, 'M')
+        self.assertEqual(obj.a, None)
+        self.assertEqual(obj.b, None)
+        self.assertFalse(hasattr(obj, 'c'))
+
+    def test_from_python(self):
         conv = self._init_modeldict_conv()
         obj = conv.model(a=1, b='2', c=3)
         value = conv.from_python(obj)
