8ed5997436b67d4f36ea1381fe58b1931a93119b
Aleksandr Osipov <osipov.as89@gmail.com>
2014-04-21 18:54:51 +0400

    Allow creation of environment without request

--- iktomi/web/app.py
+++ iktomi/web/app.py
@@ -31,14 +31,21 @@ class AppEnvironment(StorageFrame):
     def __init__(self, request, root, _parent_storage=None, **kwargs):
         StorageFrame.__init__(self, _parent_storage=_parent_storage, **kwargs)
         self.request = request
-        self.root = root.bind_to_env(self._root_storage)
-        self._route_state = RouteState(request)
+        if request:
+            self.root = root.bind_to_env(self._root_storage)
+            self._route_state = RouteState(request)
+        else:
+            self.root = root
 
     @storage_property
     def current_location(self):
         ns = getattr(self, 'namespace', '')
         url_name = getattr(self, 'current_url_name', '')
         return '.'.join(filter(None, (ns, url_name)))
+
+    @classmethod
+    def create(cls, *args, **kwargs):
+        return VersionedStorage(cls, *args, **kwargs)
 
 
 class Application(object):
