5e048cf4ed59142748147572302fb2deb158ffb2
Harut <yes@harutune.name>
2014-06-27 19:43:02 +0400

    moved docs source file to doc folder

--- doc/buildenv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo Creating venv environment
-virtualenv --no-site-packages venv
-
-echo Install PIP inside virtual environment
-./venv/bin/easy_install pip
-
-echo Installing dependencies to venv environment
-./venv/bin/pip install -E venv -r ./pipreq.txt
